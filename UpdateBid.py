22b6b42bf1eb441a552a301c2eec36619f5232b9
Conor Hayes <ch1987@live.ie>
2015-02-03 10:56:59 +0000

    Added library for zmq integration. Added method to subscribe to BidPlaced

--- UpdateBid.py
+++ UpdateBid.py
@@ -1,1 +1,32 @@
 __author__ = 'Conor'
+
+import zmq
+
+context = zmq.Context()
+SUBSCRIBER_ADDRESS = 'tcp://127.0.0.1:1111'
+
+
+class UpdateBid:
+
+    @staticmethod
+    def parse_message(message, start_tag, end_tag):
+        start_index = message.index(start_tag) + len(start_tag)
+        substring = message[start_index:]
+        end_index = substring.index(end_tag)
+        return substring[:end_index]
+
+    @staticmethod
+    def initialize_subscriber():
+        subscriber = context.socket(zmq.SUB)
+        subscriber.connect(SUBSCRIBER_ADDRESS)
+        subscriber.setsockopt(zmq.SUBSCRIBE, str.encode('BidChanged'))
+        print('Subscribed to BidChanged event...')
+
+        while True:
+            msg = subscriber.recv()
+            m = msg.decode(encoding='UTF-8')
+            print(m + ' received...')
+
+if __name__ == '__main__':
+    updater = UpdateBid()
+    updater.initialize_subscriber()
